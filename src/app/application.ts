989cdf45501a7cb6728d97f6f64940379adae55e
Dima Samoshkin <de.metro.samuel@gmail.com>
2023-01-16 02:08:10 +0400

    Выполнена настройка подключения к базе данных
    1. Создан класс database.service для подключения к базе данных и отключения от неё
    2. Добавлена вспомогательная функция для генерации URI строки
    3. Добавлена привязка для DatabaseService
    4. Выполенено подключение базы данных к application

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,5 +1,7 @@
 import { Component } from '../types/component.types.js';
 import { ConfigInterface } from '../common/config/config.interface.js';
+import { DatabaseInterface } from '../common/database-client/database.interface.js';
+import { getURI } from '../utils/db.js';
 import { inject, injectable } from 'inversify';
 import {LoggerInterface} from '../common/logger/logger.interface.js';
 import 'reflect-metadata';
@@ -9,10 +11,22 @@ export default class Application {
   constructor(
     @inject(Component.LoggerInterface) private logger: LoggerInterface,
     @inject(Component.ConfigInterface) private config: ConfigInterface,
+    @inject(Component.DatabaseInterface) private databaseClient: DatabaseInterface,
   ) {}
 
   public async init() {
     this.logger.info('Application initialization…');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
+
+
+    const uri = getURI(
+      this.config.get('DB_USER'),
+      this.config.get('DB_PASSWORD'),
+      this.config.get('DB_HOST'),
+      this.config.get('DB_PORT'),
+      this.config.get('DB_NAME'),
+    );
+
+    await this.databaseClient.connect(uri);
   }
 }
