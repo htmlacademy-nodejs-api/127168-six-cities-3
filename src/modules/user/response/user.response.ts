e94a94828c207dbbf065d63caf36b8b30b271730
Dima Samoshkin <de.metro.samuel@gmail.com>
2023-02-03 02:08:17 +0400

    Доработан UserController с учётом обновленного ExceptionFilter
    Ошибки описывают разные состояния ошибочных HTTP запросов. Добавлен метод login (будет дорабатываться позже)

---
+++ src/modules/user/response/user.response.ts
@@ -0,0 +1,16 @@
+import { Expose } from 'class-transformer';
+import { UserStatus } from '../../../types/user-status.enum.js';
+
+export default class UserResponse {
+  @Expose()
+  public username!: string;
+
+  @Expose()
+  public email!: string ;
+
+  @Expose()
+  public avatar!: string;
+
+  @Expose()
+  public userStatus!: UserStatus;
+}
