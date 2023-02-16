0e81417d16e9012b4e36088d769b0981002b81e0
Dima Samoshkin <de.metro.samuel@gmail.com>
2023-02-17 00:38:43 +0400

    Переработан обработчик login. При успешной аутентификации генерируется токен

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,4 @@
+import * as jose from 'jose';
 import { Amenities } from '../types/amenities.enum.js';
 import { City } from '../types/city.enum.js';
 import crypto from 'crypto';
@@ -59,3 +60,10 @@ export const fillDTO = <T, V>(someDTO: ClassConstructor<T>, plainObject: V) =>
 export const createErrorObject = (message: string) => ({
   error: message,
 });
+
+export const createJWT = async (algoritm: string, jwtSecret: string, payload: object): Promise<string> =>
+  new jose.SignJWT({...payload})
+    .setProtectedHeader({ alg: algoritm})
+    .setIssuedAt()
+    .setExpirationTime('2d')
+    .sign(crypto.createSecretKey(jwtSecret, 'utf-8'));
